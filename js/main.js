ebab80d8e814d8557a76e1c79c97648393e221a6
Déborah <trotadeborah@gmail.com>
2022-01-13 17:30:03 -0300

    header with mobile navigation menu

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function myFunction() {
+
+    var x = document.getElementById("navList");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+
+    }
+}
